1081bc1732322297f9a8eaded4ce0269ba83c0cb
Ben Shapira <ben@shapira.dev>
2021-12-13 15:31:43 +0200

    Refactoring RedisObjectHandler.SetId, Added Integer to the supported RedisIdFieldAttribute (#34)
    * Refactor RedisObjectHandler.SetId
    
    1.Adjusted to easier to maintain type check.
    2. Removed non working default check on object type.
    
    3.Removed idProperty.SetValue(obj, id); to avoid exceptions when property is not of type string.
    
    * Adjusted order of if statements
    
    * some updates for id generation
    
    Co-authored-by: Ben <ben.shapira@idka.ltd>
    Co-authored-by: slorello89 <42971704+slorello89@users.noreply.github.com>

---
+++ test/Redis.OM.Unit.Tests/Serialization/ObjectWithIntegerId.cs
@@ -0,0 +1,10 @@
+﻿using Redis.OM.Modeling;
+
+namespace Redis.OM.Unit.Tests
+{
+    [Document]
+    public class ObjectWithIntegerId
+    {
+        [RedisIdField] public int Id { get; set; }
+    }
+}
