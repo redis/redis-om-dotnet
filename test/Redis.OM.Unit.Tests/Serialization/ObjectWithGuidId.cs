a45213c0f8ef1a49c18c75d6fe8a3f7632266933
Steve Lorello <42971704+slorello89@users.noreply.github.com>
2022-01-31 12:28:25 -0500

    fixing UUID4 generation logic, fixing id generation of value types (#53)
    * fixing UUID4 generation logic
    * fixing id generation of value types bound to models
    * adding blurb about Ids and keys to the README.

---
+++ test/Redis.OM.Unit.Tests/Serialization/ObjectWithGuidId.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Redis.OM.Modeling;
+
+namespace Redis.OM.Unit.Tests
+{
+    [Document(IdGenerationStrategyName = nameof(Uuid4IdGenerationStrategy))]
+    public class ObjectWithGuidId
+    {
+        [RedisIdField]public Guid Id { get; set; }
+    }
+}
