58b3d3dff2c5e2e615a908de2742a95a4f227b83
Dan Walsh <dan@danwalsh.ca>
2022-02-08 07:46:52 -0500

    Exception handling for index creation/deletion (#60)
    * Added missing unknown index exception handle
    
    * Added exception handling to index creation
    
    Co-authored-by: Steve Lorello <42971704+slorello89@users.noreply.github.com>

--- test/Redis.OM.Unit.Tests/RediSearchTests/RedisIndexTests.cs
+++ test/Redis.OM.Unit.Tests/RediSearchTests/RedisIndexTests.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using Redis.OM;
-using Xunit;
 using Redis.OM.Modeling;
+using Xunit;
 
-namespace Redis.OM.Unit.Tests
+namespace Redis.OM.Unit.Tests.RediSearchTests
 {
     public class RedisIndexTests
     {        
@@ -38,7 +35,7 @@ namespace Redis.OM.Unit.Tests
         public void TestIndexSerializationHappyPath()
         {
             var expected = new[] { "TestPersonClassHappyPath-idx",
-                "ON", "Hash", "PREFIX", "1", "Redis.OM.Unit.Tests.RedisIndexTests+TestPersonClassHappyPath:", "SCHEMA",
+                "ON", "Hash", "PREFIX", "1", "Redis.OM.Unit.Tests.RediSearchTests.RedisIndexTests+TestPersonClassHappyPath:", "SCHEMA",
                 "Name", "TEXT", "SORTABLE", "Age", "NUMERIC", "SORTABLE" };
             var indexArr = typeof(TestPersonClassHappyPath).SerializeIndex();
 
@@ -69,6 +66,30 @@ namespace Redis.OM.Unit.Tests
         }
         
         [Fact]
+        public async Task TestCreateIndexAsync()
+        {
+            var host = Environment.GetEnvironmentVariable("STANDALONE_HOST_PORT") ?? "localhost";
+            var provider = new RedisConnectionProvider($"redis://{host}");
+            var connection = provider.Connection;
+            await connection.DropIndexAsync(typeof(TestPersonClassHappyPath));
+            var res = await connection.CreateIndexAsync(typeof(TestPersonClassHappyPath));            
+            Assert.True(res);
+            await connection.DropIndexAsync(typeof(TestPersonClassHappyPath));
+        }
+
+        [Fact]
+        public void TestCreateAlreadyExistingIndex()
+        {
+            var host = Environment.GetEnvironmentVariable("STANDALONE_HOST_PORT") ?? "localhost";
+            var provider = new RedisConnectionProvider($"redis://{host}");
+            var connection = provider.Connection;
+            connection.CreateIndex(typeof(TestPersonClassHappyPath));  
+            var res = connection.CreateIndex(typeof(TestPersonClassHappyPath));
+            Assert.False(res);
+            connection.DropIndex(typeof(TestPersonClassHappyPath));
+        }
+        
+        [Fact]
         public void TestDropExistingIndex()
         {
             var host = Environment.GetEnvironmentVariable("STANDALONE_HOST_PORT") ?? "localhost";
@@ -81,6 +102,18 @@ namespace Redis.OM.Unit.Tests
         }
 
         [Fact]
+        public async Task TestDropExistingIndexAsync()
+        {
+            var host = Environment.GetEnvironmentVariable("STANDALONE_HOST_PORT") ?? "localhost";
+            var provider = new RedisConnectionProvider($"redis://{host}");
+            var connection = provider.Connection;
+            await connection.DropIndexAsync(typeof(TestPersonClassHappyPath));
+            await connection.CreateIndexAsync(typeof(TestPersonClassHappyPath));            
+            var res = await connection.DropIndexAsync(typeof(TestPersonClassHappyPath));
+            Assert.True(res);
+        }
+
+        [Fact]
         public void TestDropIndexWhichDoesNotExist()
         {
             var host = Environment.GetEnvironmentVariable("STANDALONE_HOST_PORT") ?? "localhost";
@@ -88,6 +121,17 @@ namespace Redis.OM.Unit.Tests
             var connection = provider.Connection;
             connection.DropIndex(typeof(TestPersonClassHappyPath));
             var res = connection.DropIndex(typeof(TestPersonClassHappyPath));
+            Assert.False(res);
+        }
+        
+        [Fact]
+        public async Task TestDropIndexWhichDoesNotExistAsync()
+        {
+            var host = Environment.GetEnvironmentVariable("STANDALONE_HOST_PORT") ?? "localhost";
+            var provider = new RedisConnectionProvider($"redis://{host}");
+            var connection = provider.Connection;
+            await connection.DropIndexAsync(typeof(TestPersonClassHappyPath));
+            var res = await connection.DropIndexAsync(typeof(TestPersonClassHappyPath));
             Assert.False(res);
         }
     }
