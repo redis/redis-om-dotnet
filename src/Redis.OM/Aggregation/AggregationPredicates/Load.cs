b0732b8b0ba14e07dd1035280fb399c8c47221bf
Steve Lorello <42971704+slorello89@users.noreply.github.com>
2022-06-29 09:33:34 -0400

    Aggregation Load subcommand (#122)

---
+++ src/Redis.OM/Aggregation/AggregationPredicates/Load.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Redis.OM.Aggregation.AggregationPredicates
+{
+    /// <summary>
+    /// Represents a Load aggregation predicate.
+    /// </summary>
+    public class Load : IAggregationPredicate
+    {
+        private const string LoadString = "LOAD";
+        private IEnumerable<string> _properties;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Load"/> class.
+        /// </summary>
+        /// <param name="properties">The properties to load.</param>
+        public Load(IEnumerable<string> properties)
+        {
+            _properties = properties;
+        }
+
+        /// <inheritdoc />
+        public IEnumerable<string> Serialize()
+        {
+            yield return LoadString;
+            yield return _properties.Count().ToString();
+            foreach (var property in _properties)
+            {
+                yield return property;
+            }
+        }
+    }
+}
