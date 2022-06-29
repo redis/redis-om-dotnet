f66581e5a8ebefbc51b617640603e26003dd1664
Steve Lorello <42971704+slorello89@users.noreply.github.com>
2022-06-29 08:36:45 -0400

    fixing issues with async methods (#130)

--- src/Redis.OM/SearchExtensions.cs
+++ src/Redis.OM/SearchExtensions.cs
@@ -478,6 +478,42 @@ namespace Redis.OM
         }
 
         /// <summary>
+        /// Orders the collection by the provided attribute.
+        /// </summary>
+        /// <param name="source">The Redis Collection.</param>
+        /// <param name="expression">The expression.</param>
+        /// <typeparam name="T">The base type.</typeparam>
+        /// <typeparam name="TField">The field type to order by.</typeparam>
+        /// <returns>A redis collection extending the pipeline of linq expressions with the relevant SORTBY.</returns>
+        public static IRedisCollection<T> OrderBy<T, TField>(this IRedisCollection<T> source, Expression<Func<T, TField>> expression)
+            where T : notnull
+        {
+            var exp = Expression.Call(
+                null,
+                GetMethodInfo(OrderBy, source, expression),
+                new[] { source.Expression, Expression.Quote(expression) });
+            return new RedisCollection<T>((RedisQueryProvider)source.Provider, exp, source.StateManager, source.ChunkSize);
+        }
+
+        /// <summary>
+        /// Orders the collection by the provided attribute.
+        /// </summary>
+        /// <param name="source">The Redis Collection.</param>
+        /// <param name="expression">The expression.</param>
+        /// <typeparam name="T">The base type.</typeparam>
+        /// <typeparam name="TField">The field type to order by.</typeparam>
+        /// <returns>A redis collection extending the pipeline of linq expressions with the relevant SORTBY.</returns>
+        public static IRedisCollection<T> OrderByDescending<T, TField>(this IRedisCollection<T> source, Expression<Func<T, TField>> expression)
+            where T : notnull
+        {
+            var exp = Expression.Call(
+                null,
+                GetMethodInfo(OrderByDescending, source, expression),
+                new[] { source.Expression, Expression.Quote(expression) });
+            return new RedisCollection<T>((RedisQueryProvider)source.Provider, exp, source.StateManager, source.ChunkSize);
+        }
+
+        /// <summary>
         /// Get a Random sample from redis.
         /// </summary>
         /// <param name="source">The source.</param>
