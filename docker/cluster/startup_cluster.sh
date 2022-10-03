6dbee3e834d93ee10c0b2d2d692032db1dcfed51
slorello89 <steve.lorello@redislabs.com>
2022-10-03 13:18:55 -0400

    tuning

--- docker/cluster/startup_cluster.sh
+++ docker/cluster/startup_cluster.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 /opt/redis-stack/bin/redis-server /redis/redis.conf
-sleep 1
+sleep 5
 echo yes | redis-cli --cluster create 192.168.57.10:6379 192.168.57.11:6379 192.168.57.12:6379 192.168.57.13:6379 192.168.57.14:6379 192.168.57.15:6379
 sleep infinity
