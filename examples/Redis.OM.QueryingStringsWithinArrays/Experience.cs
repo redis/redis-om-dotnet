6fc876234dbc1a116f6c08a39f2a948f372998fd
Ramin-Guliyev <ra1000ishere@outlook.com>
2022-10-08 14:16:45 +0400

    Quering string array example added

---
+++ examples/Redis.OM.QueryingStringsWithinArrays/Experience.cs
@@ -0,0 +1,19 @@
+﻿using Redis.OM.Modeling;
+
+namespace Redis.OM.QueryingStringsWithinArrays;
+
+[Document(StorageType =StorageType.Json)]
+public class Experience
+{
+    [RedisIdField]
+    [Indexed]
+    public string Id { get; set; } = default!;
+
+    [Indexed]
+    public string[] Skills { get; set; } = default!;
+
+    public override string ToString()
+    {
+        return string.Join(" ", Skills.Skip(0));
+    }
+}
