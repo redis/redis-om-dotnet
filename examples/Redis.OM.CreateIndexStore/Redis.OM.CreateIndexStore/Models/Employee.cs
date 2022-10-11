4ab09cac5ea3ec3c3e927935ac4a143d0d4f662c
Rohan Edman <zulander@gmail.com>
2022-10-11 16:21:01 -0400

    Create an Index and Store Documents (#232)

---
+++ examples/Redis.OM.CreateIndexStore/Redis.OM.CreateIndexStore/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using Redis.OM.Modeling;
+
+namespace Redis.OM.CreateIndexStore.Models
+{
+    [Document(StorageType = StorageType.Json, Prefixes = new[] { "CANADA.STORE.TORONTO" }, IndexName = "Store1-idx")]
+    public class Employee
+    {
+        [RedisIdField][Indexed] public string Id { get; set; } = null!;
+
+        [Indexed] public string FullName { get; set; } = null!;
+
+        [Indexed] public int Age { get; set; }
+
+        [Indexed] public EmploymentType EmploymentType { get; set; }
+    }
+
+    public enum EmploymentType
+    {
+        FullTime,
+        PartTime
+    }
+}
