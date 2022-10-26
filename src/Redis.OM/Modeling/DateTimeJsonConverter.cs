6d0524c02bcbbd033852901f9325cabc4db2d0d7
Steve Lorello <42971704+slorello89@users.noreply.github.com>
2022-10-26 10:54:05 -0400

    adding ability to index and query datetimes (#240)
    * adding ability to index and query datetimes
    
    * fixing sortable bit of index creation

--- src/Redis.OM/Modeling/DateTimeJsonConverter.cs
+++ src/Redis.OM/Modeling/DateTimeJsonConverter.cs
@@ -12,15 +12,16 @@ namespace Redis.OM.Modeling
         /// <inheritdoc />
         public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
-            var val = reader.GetString();
+            long val = reader.TokenType == JsonTokenType.String ? long.Parse(reader.GetString() !) : reader.GetInt64();
+
             DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
-            return TimeZoneInfo.ConvertTimeFromUtc(dateTime.AddMilliseconds(long.Parse(val!)), TimeZoneInfo.Local);
+            return TimeZoneInfo.ConvertTimeFromUtc(dateTime.AddMilliseconds(val), TimeZoneInfo.Local);
         }
 
         /// <inheritdoc />
         public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
         {
-            writer.WriteStringValue(new DateTimeOffset(value).ToUnixTimeMilliseconds().ToString());
+            writer.WriteNumberValue(new DateTimeOffset(value).ToUnixTimeMilliseconds());
         }
     }
 }
