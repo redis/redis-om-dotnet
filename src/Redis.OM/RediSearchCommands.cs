ed49b6cc4bf06ce99f0412fd01e5d0fa26672383
Jeevananthan-23 <jeeva023@outlook.com>
2022-11-14 19:32:37 +0530

    Adding: Async support for suggestionmethods Refactor: GetSuggestion method Replacing: missmatch names from FTS examples

--- src/Redis.OM/RediSearchCommands.cs
+++ src/Redis.OM/RediSearchCommands.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Redis.OM.Contracts;
@@ -230,17 +229,27 @@ namespace Redis.OM
         /// <param name="withscores">Optional type also returns the score of each suggestion.</param>
         /// <param name="withpayloads">Optional type returns optional payloads saved along with the suggestions.</param>
         /// <returns>List of string suggestions for prefix.</returns>
-        public static List<string> GetSuggestion(this IRedisConnection connection, Type type, string prefix, bool fuzzy = false, int? max = 0, bool withscores = false, bool withpayloads = false)
+        public static RedisReply[] GetSuggestion(this IRedisConnection connection, Type type, string prefix, bool fuzzy = false, int? max = 0, bool withscores = false, bool withpayloads = false)
         {
-            var ret = new List<string>();
             var args = type.SerializeGetSuggestions(prefix, fuzzy, max, withscores, withpayloads);
-            var res = connection.Execute("FT.SUGGET", args).ToArray();
-            for (var i = 0; i < res.Length; i++)
-            {
-                ret.Add(res[i]);
-            }
+            return connection.Execute("FT.SUGGET", args);
+        }
 
-            return ret;
+        /// <summary>
+        /// Get a list of suggestions for a string.
+        /// </summary>
+        /// <param name="connection">the connection to redis.</param>
+        /// <param name="type">the type to get suggestion dictionary key.</param>
+        /// <param name="prefix">prefix to complete on.</param>
+        /// <param name="fuzzy">Optional type performs a fuzzy prefix search.</param>
+        /// <param name="max">Optional type limits the results to a maximum of num (default: 5).</param>
+        /// <param name="withscores">Optional type also returns the score of each suggestion.</param>
+        /// <param name="withpayloads">Optional type returns optional payloads saved along with the suggestions.</param>
+        /// <returns>List of string suggestions for prefix.</returns>
+        public static async Task<RedisReply[]> GetSuggestionAsync(this IRedisConnection connection, Type type, string prefix, bool fuzzy = false, int? max = 0, bool withscores = false, bool withpayloads = false)
+        {
+            var args = type.SerializeGetSuggestions(prefix, fuzzy, max, withscores, withpayloads);
+            return await connection.ExecuteAsync("FT.SUGGET", args);
         }
 
         /// <summary>
@@ -260,6 +269,22 @@ namespace Redis.OM
         }
 
         /// <summary>
+        /// Adds a suggestion string to an auto-suggestion dictionary.
+        /// </summary>
+        /// <param name="connection">the connection to redis.</param>
+        /// <param name="type">the type to get suggestion dictionary key.</param>
+        /// <param name="value">is suggestion string to index.</param>
+        /// <param name="score">is floating point number of the suggestion string's weight.</param>
+        /// <param name="increment">increment score value.</param>
+        /// <param name="payload">jsonpayload.</param>
+        /// <returns>A type return long.</returns>
+        public static async Task<long> AddSuggestionAsync(this IRedisConnection connection, Type type, string value, float score, bool increment = false, object? payload = null)
+        {
+            var args = type.SerializeSuggestions(value, score, increment, payload);
+            return await connection.ExecuteAsync("FT.SUGADD", args);
+        }
+
+        /// <summary>
         /// Deletes a suggestion string from an auto-suggestion dictionary.
         /// </summary>
         /// <param name="connection">the connection to redis.</param>
@@ -275,7 +300,7 @@ namespace Redis.OM
                 var result = connection.Execute("FT.SUGDEL", args);
                 if (result == 0)
                 {
-                    throw new Exception("Given suggestionString is not added to suggestion dictionary");
+                    throw new Exception($"Given {suggestionstring} is not added to suggestion dictionary");
                 }
                 else
                 {
@@ -284,7 +309,41 @@ namespace Redis.OM
             }
             catch (Exception ex)
             {
-                if (ex.Message.Contains("Given suggestionString is not added to suggestion dictionary"))
+                if (ex.Message.Contains($"Given {suggestionstring} is not added to suggestion dictionary"))
+                {
+                    return false;
+                }
+
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Deletes a suggestion string from an auto-suggestion dictionary.
+        /// </summary>
+        /// <param name="connection">the connection to redis.</param>
+        /// <param name="type">the type to get suggestion dictionary key.</param>
+        /// <param name="suggestionstring">suggestion string to index.</param>
+        /// <returns>if the string was found and deleted.</returns>
+        public static async Task<bool> DeleteSuggestionAsync(this IRedisConnection connection, Type type, string suggestionstring)
+        {
+            try
+            {
+                var key = type.SerializeSuggestions().First();
+                var args = new[] { key, suggestionstring };
+                var result = await connection.ExecuteAsync("FT.SUGDEL", args);
+                if (result == 0)
+                {
+                    throw new Exception($"Given {suggestionstring} is not added to suggestion dictionary");
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message.Contains($"Given {suggestionstring} is not added to suggestion dictionary"))
                 {
                     return false;
                 }
@@ -303,6 +362,18 @@ namespace Redis.OM
         {
             var indexName = type.SerializeSuggestions().First();
             return connection.Execute("FT.SUGLEN", indexName);
+        }
+
+        /// <summary>
+        /// Get auto-suggestion dictionary length.
+        /// </summary>
+        /// <param name="connection">the connection to redis.</param>
+        /// <param name="type">the type to get suggestion dictionary key.</param>
+        /// <returns>Returns the size of an auto-suggestion dictionary.</returns>
+        public static async Task<long> GetSuggestionLengthAsync(this IRedisConnection connection, Type type)
+        {
+            var indexName = type.SerializeSuggestions().First();
+            return await connection.ExecuteAsync("FT.SUGLEN", indexName);
         }
 
         /// <summary>
--- test/Redis.OM.Unit.Tests/AutoSuggestionTest/SuggestionFunctionalTest.cs
+++ test/Redis.OM.Unit.Tests/AutoSuggestionTest/SuggestionFunctionalTest.cs
@@ -1,7 +1,9 @@
 ﻿using Redis.OM.Contracts;
 using Redis.OM.Searching;
+using System;
 using System.Linq;
 using System.Text.Json;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace Redis.OM.Unit.Tests.AutoSuggestionTest
@@ -9,6 +11,7 @@ namespace Redis.OM.Unit.Tests.AutoSuggestionTest
     [Collection("Redis")]
     public class SuggestionFunctionalTest
     {
+        private readonly Type type = typeof(Airport);
         public SuggestionFunctionalTest(RedisSetup setup)
         {
             _connection = setup.Connection;
@@ -25,13 +28,11 @@ namespace Redis.OM.Unit.Tests.AutoSuggestionTest
             collection.Insert(airport1);
             collection.Insert(airport2);
             collection.Insert(airport3);
-            var type = typeof(Airport);
             _connection.AddSuggestion(type, airport1.Name, 1);
             _connection.AddSuggestion(type, airport1.Code, 1);
             _connection.AddSuggestion(type, airport1.State, 1);
             var listOfSuggestions = _connection.GetSuggestion(type, "De");
-            Assert.Equal(2, listOfSuggestions.Count);
-            Assert.Contains(listOfSuggestions, x=>x.Contains(airport1.State));
+            Assert.Equal(2, listOfSuggestions.Length);
         }
 
         [Fact]
@@ -44,15 +45,13 @@ namespace Redis.OM.Unit.Tests.AutoSuggestionTest
             collection.Insert(airport1);
             collection.Insert(airport2);
             collection.Insert(airport3);
-            var type = typeof(Airport);
             _connection.AddSuggestion(type, "International Airport1", 1, false, airport1);
             _connection.AddSuggestion(type, "International Airport2", 1, false, airport2);
             _connection.AddSuggestion(type, "International Airport3", 1.0f, false, airport3);
             var listOfSuggestions = _connection.GetSuggestion(type, "International", true, 3, false, true);
-            var queries = listOfSuggestions.Where(name => name.Contains('}'));
-            var query1 = queries.ToList().First();
-            var query2 = queries.ToList().Skip(1).First();
-            var query3 = queries.ToList().Skip(2).First();
+            var query1 = listOfSuggestions.Skip(1).First();
+            var query2 = listOfSuggestions.Skip(3).First();
+            var query3 = listOfSuggestions.Skip(5).First();
             var deJson1 = JsonSerializer.Deserialize<Airport>(query1);
             var deJson2 = JsonSerializer.Deserialize<Airport>(query2);
             var deJson3 = JsonSerializer.Deserialize<Airport>(query3);
@@ -71,13 +70,74 @@ namespace Redis.OM.Unit.Tests.AutoSuggestionTest
             collection.Insert(airport1);
             collection.Insert(airport2);
             collection.Insert(airport3);
-            var type = typeof(Airport);
             _connection.AddSuggestion(type, airport3.Name, 1);
             _connection.AddSuggestion(type, airport3.State, 1);
             _connection.AddSuggestion(type, airport2.Code, 1);
             _connection.DeleteSuggestion(type, "MAA");
             var addedSuggestionCount = _connection.AddSuggestion(type, "BLR", 1);
             var lengthOfAddSuggestioned = _connection.GetSuggestionLength(type);
+            Assert.Equal(lengthOfAddSuggestioned, addedSuggestionCount);
+        }
+
+        [Fact]
+        public async Task TestAddAndGetSuggestionAsync()
+        {
+            var collection = new RedisCollection<Airport>(_connection);
+            var airport1 = new Airport() { Name = "Indira Gandhi International Airport", Code = "DEL", State = "Delhi" };
+            var airport2 = new Airport() { Name = "Chennai International Airport", Code = "MAA", State = "Tamil Nadu" };
+            var airport3 = new Airport() { Name = "Kempegowda International Airport", Code = "BLR", State = "Karnataka" };
+            await collection.InsertAsync(airport1);
+            await collection.InsertAsync(airport2);
+            await collection.InsertAsync(airport3);
+            await _connection.AddSuggestionAsync(type, airport1.Name, 1);
+            await _connection.AddSuggestionAsync(type, airport1.Code, 1);
+            await _connection.AddSuggestionAsync(type, airport1.State, 1);
+            var listOfSuggestions = await _connection.GetSuggestionAsync(type, "De");
+            Assert.Equal(2, listOfSuggestions.Length);
+        }
+
+
+        [Fact]
+        public async Task TestSuggestionWithOptionalParametersAsync()
+        {
+            var collection = new RedisCollection<Airport>(_connection);
+            var airport1 = new Airport() { Name = "Indira Gandhi International Airport", Code = "DEL", State = "Delhi" };
+            var airport2 = new Airport() { Name = "Chennai International Airport", Code = "MAA", State = "Tamil Nadu" };
+            var airport3 = new Airport() { Name = "Kempegowda International Airport", Code = "BLR", State = "Karnataka" };
+            await collection.InsertAsync(airport1);
+            await collection.InsertAsync(airport2);
+            await collection.InsertAsync(airport3);
+            await _connection.AddSuggestionAsync(type, "International Airport1", 1, false, airport1);
+            await _connection.AddSuggestionAsync(type, "International Airport2", 1, false, airport2);
+            await _connection.AddSuggestionAsync(type, "International Airport3", 1.0f, false, airport3);
+            var listOfSuggestions = await _connection.GetSuggestionAsync(type, "International", true, 3, false, true);
+            var query1 = listOfSuggestions.Skip(1).First();
+            var query2 = listOfSuggestions.Skip(3).First();
+            var query3 = listOfSuggestions.Skip(5).First();
+            var deJson1 = JsonSerializer.Deserialize<Airport>(query1.ToString());
+            var deJson2 = JsonSerializer.Deserialize<Airport>(query2.ToString());
+            var deJson3 = JsonSerializer.Deserialize<Airport>(query3.ToString());
+            Assert.Equal(airport1.Id, deJson1.Id);
+            Assert.Equal(airport2.Code, deJson2.Code);
+            Assert.Equal(airport3.State, deJson3.State);
+        }
+
+        [Fact]
+        public async Task TestDeleteAndLengthOfSuggestionAsync()
+        {
+            var collection = new RedisCollection<Airport>(_connection);
+            var airport1 = new Airport() { Name = "Indira Gandhi International Airport", Code = "DEL", State = "Delhi" };
+            var airport2 = new Airport() { Name = "Chennai International Airport", Code = "MAA", State = "Tamil Nadu" };
+            var airport3 = new Airport() { Name = "Kempegowda International Airport", Code = "BLR", State = "Karnataka" };
+            await collection.InsertAsync(airport1);
+            await collection.InsertAsync(airport2);
+            await collection.InsertAsync(airport3);
+            await _connection.AddSuggestionAsync(type, airport3.Name, 1);
+            await _connection.AddSuggestionAsync(type, airport3.State, 1);
+            await _connection.AddSuggestionAsync(type, airport2.Code, 1);
+            await _connection.DeleteSuggestionAsync(type, "MAA");
+            var addedSuggestionCount = await _connection.AddSuggestionAsync(type, "BLR", 1);
+            var lengthOfAddSuggestioned = await _connection.GetSuggestionLengthAsync(type);
             Assert.Equal(lengthOfAddSuggestioned, addedSuggestionCount);
         }
     }
