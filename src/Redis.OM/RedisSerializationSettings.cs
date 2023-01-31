294ff3bf7e0e3981abb13dbed4caa52b2d273fce
Julius Mikkelä <julius.mikkela@regionstockholm.se>
2023-01-31 10:59:42 +0100

    Introduced configurable TimeZone for DateTimeJsonConverter
    - New static RedisSerializationSettings for centralized serialization config
    - UseUtcTime & UseLocalTime settings
    - Moved a lot of duplicated JsonSerializerSettings into RedisSerializationSettings

---
+++ src/Redis.OM/RedisSerializationSettings.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Redis.OM.Modeling;
+
+namespace Redis.OM
+{
+    /// <summary>
+    /// Configurable settings for how redis handles serialization.
+    /// </summary>
+    public static class RedisSerializationSettings
+    {
+        /// <summary>
+        /// <see cref="JsonSerializerOptions"/> used by redis.
+        /// </summary>
+        public static readonly JsonSerializerOptions JsonSerializerOptions = new JsonSerializerOptions()
+        {
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+        };
+
+        static RedisSerializationSettings()
+        {
+            JsonSerializerOptions.Converters.Add(new GeoLocJsonConverter());
+            JsonSerializerOptions.Converters.Add(new DateTimeJsonConverter());
+        }
+
+        /// <summary>
+        /// Gets default/assumed timezone used by redis when deserializing datetimes.
+        /// </summary>
+        public static TimeZoneInfo TimeZone { get; private set; } = TimeZoneInfo.Local;
+
+        /// <summary>
+        /// Set the default/assumed <see cref="TimeZoneInfo"/> for deserialization to <see cref="TimeZoneInfo.Utc"/> instead of <see cref="TimeZoneInfo.Local"/>.
+        /// </summary>
+        public static void UseUtcTime()
+        {
+            TimeZone = TimeZoneInfo.Utc;
+        }
+
+        /// <summary>
+        /// Set the default/assumed <see cref="TimeZoneInfo"/> for deserialization to the default of <see cref="TimeZoneInfo.Local"/>.
+        /// </summary>
+        public static void UseLocalTime()
+        {
+            TimeZone = TimeZoneInfo.Local;
+        }
+    }
+}
