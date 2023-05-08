ae260d2a18c80c222b744d46f8edf406a11c251a
slorello89 <steve.lorello@redis.com>
2023-05-08 08:17:25 -0400

    fixing issue with non-nullable numerics

---
+++ test/Redis.OM.Unit.Tests/Serialization/ObjectWithNumerics.cs
@@ -0,0 +1,28 @@
+using Redis.OM.Modeling;
+
+namespace Redis.OM.Unit.Tests;
+
+[Document(StorageType = StorageType.Json)]
+public class ObjectWithNumerics
+{
+    [Indexed]
+    public int Integer { get; set; }
+    [Indexed]
+    public byte Byte { get; set; }
+    [Indexed]
+    public sbyte SByte { get; set; }
+    [Indexed]
+    public short Short { get; set; }
+    [Indexed]
+    public ushort UShort { get; set; }
+    [Indexed]
+    public uint UInt { get; set; }
+    [Indexed]
+    public long Long { get; set; }
+    [Indexed]
+    public ulong ULong { get; set; }
+    [Indexed]
+    public double Double { get; set; }
+    [Indexed]
+    public float Float { get; set; }
+}
