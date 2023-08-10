e2539d50d8378222cfdf0b5304c337f08cf1babd
slorello89 <steve.lorello@redis.com>
2023-08-10 14:17:45 -0400

    restoring program.cs

--- test/Redis.OM.Test.ConsoleApp/Program.cs
+++ test/Redis.OM.Test.ConsoleApp/Program.cs
@@ -20,15 +20,14 @@ namespace Redis.OM.Test.ConsoleApp
             [Indexed(Aggregatable = true)] public int Age { get; set; }
             [Indexed(Aggregatable = true)] public GeoLoc Home { get; set; }
         }
-
-        private static void Main(string[] args)
+        static void Main(string[] args)
         {
             // connect
             var provider = new RedisConnectionProvider("redis://localhost:6379");
             var connection = provider.Connection;
             var customers = provider.RedisCollection<Customer>();
             var customerAggregations = provider.AggregationSet<Customer>();
-
+            
             // Create index
             connection.CreateIndex(typeof(Customer));
 
@@ -36,32 +35,31 @@ namespace Redis.OM.Test.ConsoleApp
             var indexinfo = connection.GetIndexInfo(typeof(Customer));
 
             // Insert Object
-            customers.Insert(new Customer
-            {
+            customers.Insert(new Customer{
                 FirstName = "James",
                 LastName = "Bond",
                 Email = "bondjamesbond@email.com",
                 Age = 68
             });
-
+            
             // query
             // Find all customers who's last name is "Bond"
             var res = customers.Where(x => x.LastName == "Bond").ToList();
-
+            
             // Find all customers who's last name is Bond OR who's age is greater than 65
             customers.Where(x => x.LastName == "Bond" || x.Age > 65);
-
+            
             // Find all customer's who's last name is Bond AND who's first name is James
             customers.Where(x => x.LastName == "Bond" && x.FirstName == "James");
-
+            
             // Get Average Age
             customerAggregations.Average(x => x.RecordShell.Age);
-
+            
             // Format Customer Full Names
             customerAggregations.Apply(x => string.Format("{0} {1}", x.RecordShell.FirstName, x.RecordShell.LastName),
                 "FullName");
-
-            // Get Customer Distance from Mall of America.
+            
+            // Get Customer Distance from Mall of America. 
             customerAggregations.Apply(x => ApplyFunctions.GeoDistance(x.RecordShell.Home, -93.241786, 44.853816),
                 "DistanceToMall");
         }
