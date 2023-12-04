2c049f1d1cfcadc322e92bec7d29372e05482704
slorello89 <steve.lorello@redis.com>
2023-12-04 07:36:52 -0500

    fixing test host for Vectorizer tests

--- src/Redis.OM.Vectorizers.Resnet18/ImageVectorizerAttribute.cs
+++ src/Redis.OM.Vectorizers.Resnet18/ImageVectorizerAttribute.cs
@@ -8,6 +8,5 @@ public class ImageVectorizerAttribute : VectorizerAttribute<string>
     public override VectorType VectorType => Vectorizer.VectorType;
     public override int Dim => Vectorizer.Dim;
     public override byte[] Vectorize(object obj) => Vectorizer.Vectorize((string)obj);
-
     public override IVectorizer<string> Vectorizer { get; } = new ImageVectorizer();
 }
