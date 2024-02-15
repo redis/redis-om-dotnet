9975c58389c6e60a7dd2b04abb8fef557dea30d4
slorello89 <steve.lorello@redis.com>
2024-02-15 08:53:23 -0500

    fixing search test

--- test/Redis.OM.Unit.Tests/CoreTests.cs
+++ test/Redis.OM.Unit.Tests/CoreTests.cs
@@ -476,7 +476,7 @@ namespace Redis.OM.Unit.Tests
 
             await Task.WhenAll(tasks);
 
-            var ex = await Assert.ThrowsAsync<TimeoutException>(async () => await collection.Take(10000).ToListAsync());
+            var ex = await Assert.ThrowsAsync<Exception>(async () => await collection.Take(10000).ToListAsync());
             Assert.Equal("Encountered timeout when searching - check the duration of your query.", ex.Message);
         }
     }
