08b6ad5eecb8b7e7dab106aa6d6c2b5f33853556
Steve Lorello <42971704+slorello89@users.noreply.github.com>
2024-06-06 16:34:21 -0400

    fixing pull models file (#451)

--- fetch-models.sh
+++ fetch-models.sh
@@ -1,9 +1,11 @@
-#!/bin/bash
-
+mkdir -p ./src/Redis.OM.Vectorizers.AllMiniLML6V2/Resources && \
 curl -o ./src/Redis.OM.Vectorizers.AllMiniLML6V2/Resources/model.onnx https://storage.googleapis.com/slorello/model.onnx
 
+mkdir -p ./src/Redis.OM.Vectorizers.AllMiniLML6V2/Resources && \
 curl -o ./src/Redis.OM.Vectorizers.AllMiniLML6V2/Resources/vocab.txt https://storage.googleapis.com/slorello/vocab.txt
 
+mkdir -p ./src/Redis.OM.Vectorizers.Resnet18/Resources/ResNet18Onnx && \
 curl -o ./src/Redis.OM.Vectorizers.Resnet18/Resources/ResNet18Onnx/ResNet18.onnx  https://storage.googleapis.com/slorello/ResNet18.onnx
 
+mkdir -p ./src/Redis.OM.Vectorizers.Resnet18/Resources/ResNetPrepOnnx && \
 curl -o ./src/Redis.OM.Vectorizers.Resnet18/Resources/ResNetPrepOnnx/ResNetPreprocess.onnx  https://storage.googleapis.com/slorello/ResNetPreprocess.onnx
