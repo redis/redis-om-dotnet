6627acda7833a49b35579b4a8eab5a8dc610d886
slorello89 <steve.lorello@redis.com>
2024-07-10 16:50:27 -0400

    including saveState

--- src/Redis.OM/Searching/RedisQueryProvider.cs
+++ src/Redis.OM/Searching/RedisQueryProvider.cs
@@ -106,7 +106,7 @@ namespace Redis.OM.Searching
             where TElement : notnull
         {
             var booleanExpression = expression as Expression<Func<TElement, bool>>;
-            return new RedisCollection<TElement>(this, expression, StateManager, booleanExpression, true, _chunkSize);
+            return new RedisCollection<TElement>(this, expression, StateManager, booleanExpression, _saveState, _chunkSize);
         }
 
         /// <inheritdoc/>
