ac27b3c988c8f7b3785bdef97e2c95627cd9e24b
FICTURE7 <ficture7@gmail.com>
2026-01-27 14:59:37 +0400

    Add `INDEXMISSING` numeric field tests

--- test/Redis.OM.Unit.Tests/SearchJsonTests/RedisJsonIndexTests.cs
+++ test/Redis.OM.Unit.Tests/SearchJsonTests/RedisJsonIndexTests.cs
@@ -7,7 +7,6 @@ namespace Redis.OM.Unit.Tests.SearchJsonTests
 {
     public class RedisJsonIndexTests
     {
-        
         [Document(IndexName = "person-idx", StorageType = StorageType.Json)]
         public class Person
         {
@@ -21,7 +20,25 @@ namespace Redis.OM.Unit.Tests.SearchJsonTests
             public double Height { get; set; }
             public string[] NickNames { get; set; }
         }
-        
+
+        [Document(IndexName = "person-idx", StorageType = StorageType.Json)]
+        public class PersonWithIndexedNullableNumerics
+        {
+            [Searchable(Sortable = true)]
+            public string Name { get; set; }
+            [Indexed]
+            public string Tag { get; set; }
+            [Indexed(Sortable = true)]
+            public int Age { get; set; }
+            [Indexed(Sortable = true)]
+            public double Height { get; set; }
+            public string[] NickNames { get; set; }
+            [Indexed]
+            public int? Code { get; set; }
+            [Indexed]
+            public DateTime? UpdatedAt { get; set; }
+        }
+
         [Document(IndexName = "person-idx", StorageType = StorageType.Json)]
         public class PersonWithIndexedNickNames
         {
@@ -42,124 +59,131 @@ namespace Redis.OM.Unit.Tests.SearchJsonTests
         {
             [Searchable(Sortable = true)]
             public string Name { get; set; }
-            
+
             [Indexed]
             public string Tag { get; set; }
-            
+
             [Indexed(Sortable = true)]
             public int Age { get; set; }
-            
+
             [Indexed(Sortable = true)]
             public double Height { get; set; }
-            
+
             public string[] NickNames { get; set; }
-            
+
             [Indexed(JsonPath = "$.ZipCode")]
             [Indexed(JsonPath = "$.City")]
             [Searchable(JsonPath = "$.StreetName", Aggregatable = true)]
             public Address Address { get; set; }
-        
+
             [Indexed(CascadeDepth = 1)]
             public Address WorkAddress { get; set; }
         }
-        
+
         [Document(IndexName = "person-idx", StorageType = StorageType.Json)]
         public class NestedPersonCascade2
         {
             [Searchable(Sortable = true)]
             public string Name { get; set; }
-            
+
             [Indexed]
             public string Tag { get; set; }
-            
+
             [Indexed(Sortable = true)]
             public int Age { get; set; }
-            
+
             [Indexed(Sortable = true)]
             public double Height { get; set; }
-            
+
             public string[] NickNames { get; set; }
-            
+
             [Indexed(JsonPath = "$.ZipCode")]
             [Indexed(JsonPath = "$.City")]
             [Searchable(JsonPath = "$.StreetName", Aggregatable = true)]
             public Address Address { get; set; }
-        
+
             [Indexed(CascadeDepth = 2)]
             public Address WorkAddress { get; set; }
         }
-
-        
-        private void Setup() 
-        {
-            var host = Environment.GetEnvironmentVariable("REDIS_HOST") ?? "localhost";
-            var conf = new RedisConnectionConfiguration();
-            conf.Host = host;
-            var connection = conf.Connect();
-            connection.DropIndexAndAssociatedRecords(typeof(NestedPerson));
-            connection.CreateIndex(typeof(NestedPerson));
-        }        
 
         [Fact]
         public void TestIndexSerializationHappyPath()
         {
             var expected = new[] { "person-idx",
                 "ON", "Json", "PREFIX", "1", "Redis.OM.Unit.Tests.SearchJsonTests.RedisJsonIndexTests+Person:", "SCHEMA",
-                "$.Name", "AS", "Name", "TEXT", "INDEXMISSING", "INDEXEMPTY", "SORTABLE", 
+                "$.Name", "AS", "Name", "TEXT", "INDEXMISSING", "INDEXEMPTY", "SORTABLE",
                 "$.Tag", "AS","Tag","TAG", "SEPARATOR", "|", "INDEXMISSING", "INDEXEMPTY",
-                "$.Age", "AS", "Age", "NUMERIC", "SORTABLE", 
+                "$.Age", "AS", "Age", "NUMERIC", "SORTABLE",
                 "$.Height", "AS", "Height", "NUMERIC", "SORTABLE" };
             var indexArr = typeof(Person).SerializeIndex();
 
-            for(var i = 0; i < indexArr.Length; i++)
+            for (var i = 0; i < indexArr.Length; i++)
             {
                 Assert.Equal(expected[i], indexArr[i]);
             }
-            
         }
-        
+
+        [Fact]
+        public void TestIndexSerializationNullableNumeric()
+        {
+            var expected = new[] { "person-idx",
+                "ON", "Json", "PREFIX", "1", "Redis.OM.Unit.Tests.SearchJsonTests.RedisJsonIndexTests+PersonWithIndexedNullableNumerics:", "SCHEMA",
+                "$.Name", "AS", "Name", "TEXT", "INDEXMISSING", "INDEXEMPTY", "SORTABLE",
+                "$.Tag", "AS","Tag","TAG", "SEPARATOR", "|", "INDEXMISSING", "INDEXEMPTY",
+                "$.Age", "AS", "Age", "NUMERIC", "SORTABLE",
+                "$.Height", "AS", "Height", "NUMERIC", "SORTABLE",
+                "$.Code", "AS", "Code", "NUMERIC", "INDEXMISSING",
+                "$.UpdatedAt", "AS", "UpdatedAt", "NUMERIC", "INDEXMISSING" };
+            var indexArr = typeof(PersonWithIndexedNullableNumerics).SerializeIndex();
+
+            for (var i = 0; i < indexArr.Length; i++)
+            {
+                Assert.Equal(expected[i], indexArr[i]);
+            }
+        }
+
         [Fact]
         public void TestIndexSerializationNestedObject()
         {
             var expected = new[] { "person-idx",
                 "ON", "Json", "PREFIX", "1", "Redis.OM.Unit.Tests.SearchJsonTests.RedisJsonIndexTests+NestedPerson:", "SCHEMA",
-                "$.Name", "AS", "Name", "TEXT", "SORTABLE", "INDEXMISSING", "INDEXEMPTY", 
+                "$.Name", "AS", "Name", "TEXT", "SORTABLE", "INDEXMISSING", "INDEXEMPTY",
                 "$.Tag", "AS","Tag","TAG", "SEPARATOR", "|", "INDEXMISSING", "INDEXEMPTY",
-                "$.Age", "AS", "Age", "NUMERIC","SORTABLE", 
-                "$.Height", "AS", "Height", "NUMERIC", "SORTABLE", 
+                "$.Age", "AS", "Age", "NUMERIC","SORTABLE",
+                "$.Height", "AS", "Height", "NUMERIC", "SORTABLE",
                 "$.Address.ZipCode", "AS", "Address_ZipCode", "TAG", "SEPARATOR", "|", "INDEXMISSING", "INDEXEMPTY",
-                "$.Address.City", "AS", "Address_City", "TAG", "SEPARATOR", "|", "INDEXMISSING", "INDEXEMPTY", 
+                "$.Address.City", "AS", "Address_City", "TAG", "SEPARATOR", "|", "INDEXMISSING", "INDEXEMPTY",
                 "$.Address.StreetName", "AS", "Address_StreetName", "TEXT", "SORTABLE", "INDEXMISSING", "INDEXEMPTY",
                 "$.WorkAddress.City", "AS","WorkAddress_City", "TAG", "SEPARATOR", "|", "INDEXMISSING", "INDEXEMPTY",
                 "$.WorkAddress.AddressType", "AS", "WorkAddress_AddressType", "TAG", "INDEXMISSING", "INDEXEMPTY",
                 "$.WorkAddress.State", "AS", "WorkAddress_State", "TAG", "SEPARATOR", "|", "INDEXMISSING", "INDEXEMPTY",
-                "$.WorkAddress.Location", "AS", "WorkAddress_Location", "GEO", 
+                "$.WorkAddress.Location", "AS", "WorkAddress_Location", "GEO",
                 "$.WorkAddress.HouseNumber", "AS", "WorkAddress_HouseNumber", "NUMERIC",
                 "$.WorkAddress.Boolean", "AS", "WorkAddress_Boolean", "TAG", "SEPARATOR", "|", "INDEXMISSING", "INDEXEMPTY",
                 "$.WorkAddress.Ulid", "AS", "WorkAddress_Ulid", "TAG", "SEPARATOR", "|", "INDEXMISSING", "INDEXEMPTY",
                 "$.WorkAddress.Guid", "AS", "WorkAddress_Guid", "TAG", "SEPARATOR", "|", "INDEXMISSING", "INDEXEMPTY",
-                
+
             };
             var indexArr = typeof(NestedPerson).SerializeIndex();
-            Assert.Equal(expected.OrderBy(x=>x), indexArr.OrderBy(x=>x));
+            Assert.Equal(expected.OrderBy(x => x), indexArr.OrderBy(x => x));
         }
-        
+
         [Fact]
         public void TestIndexSerializationNestedObjectCascade2()
         {
             var expected = new[] { "person-idx",
                 "ON", "Json", "PREFIX", "1", "Redis.OM.Unit.Tests.SearchJsonTests.RedisJsonIndexTests+NestedPersonCascade2:", "SCHEMA",
-                "$.Name", "AS", "Name", "TEXT", "SORTABLE", "INDEXMISSING", "INDEXEMPTY", 
+                "$.Name", "AS", "Name", "TEXT", "SORTABLE", "INDEXMISSING", "INDEXEMPTY",
                 "$.Tag", "AS","Tag","TAG", "SEPARATOR", "|", "INDEXMISSING", "INDEXEMPTY",
-                "$.Age", "AS", "Age", "NUMERIC","SORTABLE", 
-                "$.Height", "AS", "Height", "NUMERIC", "SORTABLE", 
-                "$.Address.ZipCode", "AS", "Address_ZipCode", "TAG", "SEPARATOR", "|", "INDEXMISSING", "INDEXEMPTY", 
-                "$.Address.City", "AS", "Address_City", "TAG", "SEPARATOR", "|", "INDEXMISSING", "INDEXEMPTY", 
+                "$.Age", "AS", "Age", "NUMERIC","SORTABLE",
+                "$.Height", "AS", "Height", "NUMERIC", "SORTABLE",
+                "$.Address.ZipCode", "AS", "Address_ZipCode", "TAG", "SEPARATOR", "|", "INDEXMISSING", "INDEXEMPTY",
+                "$.Address.City", "AS", "Address_City", "TAG", "SEPARATOR", "|", "INDEXMISSING", "INDEXEMPTY",
                 "$.Address.StreetName", "AS", "Address_StreetName", "TEXT", "SORTABLE", "INDEXMISSING", "INDEXEMPTY",
-                "$.WorkAddress.City", "AS","WorkAddress_City", "TAG", "SEPARATOR", "|", "INDEXMISSING", "INDEXEMPTY",  
+                "$.WorkAddress.City", "AS","WorkAddress_City", "TAG", "SEPARATOR", "|", "INDEXMISSING", "INDEXEMPTY",
                 "$.WorkAddress.AddressType", "AS", "WorkAddress_AddressType", "TAG", "INDEXMISSING", "INDEXEMPTY",
                 "$.WorkAddress.State", "AS", "WorkAddress_State", "TAG", "SEPARATOR", "|", "INDEXMISSING", "INDEXEMPTY",
-                "$.WorkAddress.ForwardingAddress.City", "AS", "WorkAddress_ForwardingAddress_City", "TAG", "SEPARATOR", "|", "INDEXMISSING", "INDEXEMPTY", 
+                "$.WorkAddress.ForwardingAddress.City", "AS", "WorkAddress_ForwardingAddress_City", "TAG", "SEPARATOR", "|", "INDEXMISSING", "INDEXEMPTY",
                 "$.WorkAddress.ForwardingAddress.AddressType", "AS", "WorkAddress_ForwardingAddress_AddressType", "TAG", "INDEXMISSING", "INDEXEMPTY",
                 "$.WorkAddress.ForwardingAddress.State", "AS", "WorkAddress_ForwardingAddress_State", "TAG", "SEPARATOR", "|", "INDEXMISSING", "INDEXEMPTY",
                 "$.WorkAddress.ForwardingAddress.Location", "AS", "WorkAddress_ForwardingAddress_Location", "GEO",
@@ -167,15 +191,15 @@ namespace Redis.OM.Unit.Tests.SearchJsonTests
                 "$.WorkAddress.ForwardingAddress.Boolean", "AS", "WorkAddress_ForwardingAddress_Boolean", "TAG", "SEPARATOR", "|", "INDEXMISSING", "INDEXEMPTY",
                 "$.WorkAddress.ForwardingAddress.Ulid", "AS", "WorkAddress_ForwardingAddress_Ulid", "TAG", "SEPARATOR", "|", "INDEXMISSING", "INDEXEMPTY",
                 "$.WorkAddress.ForwardingAddress.Guid", "AS", "WorkAddress_ForwardingAddress_Guid", "TAG", "SEPARATOR", "|", "INDEXMISSING", "INDEXEMPTY",
-                "$.WorkAddress.Location", "AS", "WorkAddress_Location", "GEO", 
+                "$.WorkAddress.Location", "AS", "WorkAddress_Location", "GEO",
                 "$.WorkAddress.HouseNumber", "AS", "WorkAddress_HouseNumber", "NUMERIC",
                 "$.WorkAddress.Boolean", "AS", "WorkAddress_Boolean", "TAG", "SEPARATOR", "|", "INDEXMISSING", "INDEXEMPTY",
                 "$.WorkAddress.Ulid", "AS", "WorkAddress_Ulid", "TAG", "SEPARATOR", "|", "INDEXMISSING", "INDEXEMPTY",
                 "$.WorkAddress.Guid", "AS", "WorkAddress_Guid", "TAG", "SEPARATOR", "|", "INDEXMISSING", "INDEXEMPTY",
-                
+
             };
             var indexArr = typeof(NestedPersonCascade2).SerializeIndex();
-            Assert.Equal(expected.OrderBy(x=>x), indexArr.OrderBy(x=>x));
+            Assert.Equal(expected.OrderBy(x => x), indexArr.OrderBy(x => x));
         }
 
         [Fact]
@@ -183,20 +207,20 @@ namespace Redis.OM.Unit.Tests.SearchJsonTests
         {
             var expected = new[] { "person-idx",
                 "ON", "Json", "PREFIX", "1", "Redis.OM.Unit.Tests.SearchJsonTests.RedisJsonIndexTests+PersonWithIndexedNickNames:", "SCHEMA",
-                "$.Name", "AS", "Name", "TEXT", "INDEXMISSING", "INDEXEMPTY", "SORTABLE", 
+                "$.Name", "AS", "Name", "TEXT", "INDEXMISSING", "INDEXEMPTY", "SORTABLE",
                 "$.Tag", "AS","Tag","TAG", "SEPARATOR", "|", "INDEXMISSING", "INDEXEMPTY",
-                "$.Age", "AS", "Age", "NUMERIC","SORTABLE", 
+                "$.Age", "AS", "Age", "NUMERIC","SORTABLE",
                 "$.Height", "AS", "Height", "NUMERIC", "SORTABLE",
                 "$.NickNames[*]", "AS","NickNames","TAG",
-                
+
             };
             var indexArr = typeof(PersonWithIndexedNickNames).SerializeIndex();
 
-            for(var i = 0; i < expected.Length; i++)
+            for (var i = 0; i < expected.Length; i++)
             {
                 Assert.Equal(expected[i], indexArr[i]);
             }
         }
-        
+
     }
 }
